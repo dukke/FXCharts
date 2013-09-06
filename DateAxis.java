d1a2b03df19519019e418da7ece9d097d27ceb09
dukke <pedro.duquevieira@gmail.com>
2013-09-06 02:25:01 +0100

    cleaned up some comments

--- DateAxis.java
+++ DateAxis.java
@@ -31,28 +31,14 @@ import java.util.*;
 */
 public class DateAxis extends ValueAxis<Long> {
 
-    /** We use these for auto ranging to pick a user friendly tick unit. We handle tick units in the range of 1e-10 to 1e+12 */
+    /** We use these for auto ranging to pick a user friendly tick unit. */
     private static final double[] TICK_UNIT_DEFAULTS = {
             1296000000
     };
-//    private static final double[] TICK_UNIT_DEFAULTS = {
-//            1.0E-10d, 2.5E-10d, 5.0E-10d, 1.0E-9d, 2.5E-9d, 5.0E-9d, 1.0E-8d, 2.5E-8d, 5.0E-8d, 1.0E-7d, 2.5E-7d, 5.0E-7d,
-//            1.0E-6d, 2.5E-6d, 5.0E-6d, 1.0E-5d, 2.5E-5d, 5.0E-5d, 1.0E-4d, 2.5E-4d, 5.0E-4d, 0.0010d, 0.0025d, 0.0050d,
-//            0.01d, 0.025d, 0.05d, 0.1d, 0.25d, 0.5d, 1.0d, 2.5d, 5.0d, 10.0d, 25.0d, 50.0d, 100.0d, 250.0d, 500.0d,
-//            1000.0d, 2500.0d, 5000.0d, 10000.0d, 25000.0d, 50000.0d, 100000.0d, 250000.0d, 500000.0d, 1000000.0d,
-//            2500000.0d, 5000000.0d, 1.0E7d, 2.5E7d, 5.0E7d, 1.0E8d, 2.5E8d, 5.0E8d, 1.0E9d, 2.5E9d, 5.0E9d, 1.0E10d,
-//            2.5E10d, 5.0E10d, 1.0E11d, 2.5E11d, 5.0E11d, 1.0E12d, 2.5E12d, 5.0E12d
-//    };
-    /** These are matching decimal formatter strings */
+
+    /** These are matching date formatter strings */
     private static final String[] TICK_UNIT_FORMATTER_DEFAULTS = {"MM/dd/yy"};
-//    private static final String[] TICK_UNIT_FORMATTER_DEFAULTS = {"0.0000000000", "0.00000000000", "0.0000000000",
-//            "0.000000000", "0.0000000000", "0.000000000",
-//            "0.00000000", "0.000000000", "0.00000000",
-//            "0.0000000", "0.00000000", "0.0000000", "0.000000",
-//            "0.0000000", "0.000000", "0.00000", "0.000000",
-//            "0.00000", "0.0000", "0.00000", "0.0000", "0.000",
-//            "0.0000", "0.000", "0.00", "0.000", "0.00", "0.0",
-//            "0.00", "0.0", "0", "0.0", "0", "#,##0"};
+
 
     private Object currentAnimationID;
     private final ChartLayoutAnimator animator = new ChartLayoutAnimator(this);
